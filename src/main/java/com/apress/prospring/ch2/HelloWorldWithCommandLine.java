f7dcdd496c413751b5560a23ce1daa6c5bc1cce5
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-07-05 21:58:56 +0300

    Add usage of command line to change message

---
+++ src/main/java/com/apress/prospring/ch2/HelloWorldWithCommandLine.java
@@ -0,0 +1,14 @@
+package com.apress.prospring.ch2;
+
+/**
+ * Created by Dmytro on 05.07.16.
+ */
+public class HelloWorldWithCommandLine {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
