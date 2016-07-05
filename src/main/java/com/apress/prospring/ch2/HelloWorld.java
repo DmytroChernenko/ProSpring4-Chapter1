ad11cff1f794c183def7294648d9785435d9d425
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-07-05 21:57:13 +0300

    Initial commit Hello World in Java

---
+++ src/main/java/com/apress/prospring/ch2/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.apress.prospring.ch2;
+
+/**
+ * Created by Dmytro on 05.07.16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
